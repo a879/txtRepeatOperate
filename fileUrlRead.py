b0277660abbee49e706182eab30c2d728036aac7
knowto <963351665@qq.com>
2022-11-27 01:44:24 +0800

    添加多次循环处理

--- fileUrlRead.py
+++ fileUrlRead.py
@@ -4,19 +4,35 @@
 # fileUrl = 'C:/Users/knowto/Downloads/cdk.txt'
 
 fileUrl = str(input('输入文件路径'))
-oldWord = input('输入需要修改的内容（文字和符号）')
-newWord = input('输入新的内容')
+# isRemake = int(input('是否修改原文件： 1不修改，新建文件，2原文基础上换行添加修改后的内容'))
+
+# if isRemake == 1:
+#     mode = 'r+'
+# else:
+#     mode = 'w+'
 
 file1 = open(fileUrl, mode='r+', encoding="utf-8")
 file1.writelines('\n\n')
-# sha1.txt
-d = []
-for w in file1.readlines():
-    d.append(w)
+# 使用try with， 防止代码异常，close方法没有执行
+try:
+    while True:
+        oldWord = input('输入需要修改的内容（文字和符号）')
 
-for s in d:
-    s = s.replace(' ', '')
-    s = s.replace(oldWord, newWord)
-    file1.writelines('{content}'.format(content=s))
+        if oldWord:
+            newWord = input('输入新的内容')
+            # sha1.txt
+            d = []
+            for w in file1.readlines():
+                d.append(w)
 
-file1.close()
+            for s in d:
+                s = s.replace(' ', '')
+                s = s.replace(oldWord, newWord)
+                file1.writelines('{content}'.format(content=s))
+
+            continue
+        else:
+            print('jieshu')
+            break
+finally:
+    file1.close()
